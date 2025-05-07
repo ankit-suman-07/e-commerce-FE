76e5b938450c8061efebb13d9755c6425588db1d
Ankit Suman <105882103+ankit-suman-07@users.noreply.github.com>
2025-05-08 01:32:07 +0530

    Align image for product page

--- src/pages/ProductDetails.js
+++ src/pages/ProductDetails.js
@@ -1,18 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
+import './product-details.css'; // Assuming you have some CSS for styling
 
 const ProductDetails = () => {
   const product = useSelector((state) => state.product.selectedProduct);
   const navigate = useNavigate();
 
-  if (!product) return <p className="p-4">No product selected.</p>;
+  const [image, setImage] = useState(product.imageUrl || '');
+
+  if (!product) return <p>No product selected.</p>;
 
   return (
-    <div >
-      <button onClick={() => navigate('/')} >Back to Search</button>
-      <div>
-        <img src={product.imageUrl} alt={product.name}/>
+    <div className='product-page' >
+      <button onClick={() => navigate('/')} className='back-btn' >Back to Search</button>
+      <div className='product-page-image' >
+        <div className='product-image-main' >
+          <img src={image} alt={product.name}/>
+        </div>
+        <div className='product-image-secondary' >
+          <img src={image} alt={product.name} onClick={() => setImage(image)} />
+          <img src={image} alt={product.name} onClick={() => setImage(image)} />
+          <img src={image} alt={product.name} onClick={() => setImage(image)} />
+          <img src={image} alt={product.name} onClick={() => setImage(image)} />
+        </div>
+      </div>
+      
+      <div className='product-page-details' >
+        
         <div>
           <h1>{product.name}</h1>
           <p className="mb-1">Price: ${product.price}</p>
