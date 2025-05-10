1dcb0ff24571a2dfc451ee4c815985f20c799494
Ankit Suman <105882103+ankit-suman-07@users.noreply.github.com>
2025-05-10 19:23:32 +0530

    Create temp cart page

---
+++ src/pages/cart/Cart.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./Cart.css";
+import Neutral from '../../assets/neutral.png';
+
+const Cart = () => {
+  return (
+    <div className='cart-page' >
+        <div className='cart-demo-text' >
+            <img src={Neutral} alt="cart" />
+            <span>This page is still being built.</span>
+        </div>
+    </div>
+  )
+}
+
+export default Cart
