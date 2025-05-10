1f52039f3bf8c19f222dc9a146be31e44d27965b
Ankit Suman <105882103+ankit-suman-07@users.noreply.github.com>
2025-05-10 19:58:29 +0530

    Create routes for auth pages

---
+++ src/pages/auth/login.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./auth.css";
+
+const LogIn = () => {
+  return (
+    <div className='auth-page' >
+        LogIn
+    </div>
+  )
+}
+
+export default LogIn
---
+++ src/pages/auth/signup.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./auth.css";
+
+const SignUp = () => {
+  return (
+    <div className='auth-page' >
+        SignUp
+    </div>
+  )
+}
+
+export default SignUp
